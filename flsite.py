9299bc9188682283ffa093e6610e5787bd89a942
alexsks65536 <megbyte@mail.ru>
2022-11-11 00:04:03 +0300

    Подключение внешних ресурсов и работа с формами

--- flsite.py
+++ flsite.py
@@ -1,15 +1,18 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-menu = ["Установка", "Первое приложение", "Обратная связь"]
+menu = [{"name": "Установка", "url": "install-flask"},
+        {"name": "Первое приложение", "url": "first-app"},
+        {"name": "Обратная связь", "url": "contact"}
+        ]
 
 
 @app.route("/index")
 @app.route("/")
 def index():
     print(url_for('index'))
-    return render_template('index.html', menu=menu)
+    return render_template('base.html', menu=menu)
 
 
 @app.route("/about")
@@ -21,6 +24,14 @@ def about():
 @app.route("/profile/<username>")  # int: float: path:
 def profile(username):
     return f"Пользователь: {username}"
+
+
+@app.route("/contact", methods=['POST', 'GET'])
+def contact():
+    if request.method == 'POST':
+        print(request.form)
+    print(url_for('contact'))
+    return render_template('contact.html', title='Обратная связь', menu=menu)
 
 
 if __name__ == "__main__":

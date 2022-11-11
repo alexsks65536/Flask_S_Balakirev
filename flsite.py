dbbd15b9df04e2d2a9b71d486c9f4c0c6a2ba95b
alexsks65536 <megbyte@mail.ru>
2022-11-11 23:12:13 +0300

    Создание БД, установление и разрыв соединения при запросах

--- flsite.py
+++ flsite.py
@@ -1,7 +1,55 @@
-from flask import Flask, render_template, url_for, request, flash, session, redirect, abort
+import sqlite3
+import os
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
+
+
+# конфигурация
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = '9XuKXXJp2bwGzdToyAN%Pb*cqE0cvZezNI'
+
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'oIVpBhUCgYEAzd2qlJTnDkiVe7rAlpXZ9I8GqD5n3A'
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+# app.config['SECRET_KEY'] = 'oIVpBhUCgYEAzd2qlJTnDkiVe7rAlpXZ9I8GqD5n3A'
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    """Вспомогательная функция для создания таблиц БД"""
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    """Соединение с БД, если оно еще не установлено"""
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.route("/")
+def index():
+    db = get_db()
+    return render_template('index.html', menu=[])
+
+
+@app.teardown_appcontext
+def close_db(error):
+    """Закрываем соединение с БД, если оно было установлено"""
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
 
 menu = [{"name": "Установка", "url": "install-flask"},
         {"name": "Первое приложение", "url": "first-app"},
@@ -9,11 +57,11 @@ menu = [{"name": "Установка", "url": "install-flask"},
         ]
 
 
-@app.route("/index")
-@app.route("/")
-def index():
-    print(url_for('index'))
-    return render_template('base.html', menu=menu)
+# @app.route("/index")
+# @app.route("/")
+# def index():
+#     print(url_for('index'))
+#     return render_template('base.html', menu=menu)
 
 
 @app.route("/about")
